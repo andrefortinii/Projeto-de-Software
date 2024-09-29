c7590dcead3a31bdde9d92c2dd5296a55ade34f9
André Fortini <118738072+andrefortinii@users.noreply.github.com>
2024-09-29 10:20:57 -0300

    Add files via upload

---
+++ projeto/testinhe/src/Router.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import { Feed } from "./pages/Feed";
+import CreatePost from "./pages/CreatePost";
+import { UpdatePost } from "./pages/UpdatePost";
+import { OnePost } from "./pages/OnePost";
+import Cadastro from './pages/Cadastro';
+import Login from './pages/Login';
+import useAuth from "./hooks/useAuth";
+
+const Private = ({ Item }) => {
+  const { signed } = useAuth();
+  return signed ? <Item /> : <Login />;
+};
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/feed" element={<Feed />} />
+      <Route path="/createPost" element={<CreatePost />} />
+      <Route path="/" element={<Cadastro />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/updatePost/:id" element={<UpdatePost />} />
+      <Route path="/post/:id" element={<OnePost />} />
+    </Routes>
+  );
+}
