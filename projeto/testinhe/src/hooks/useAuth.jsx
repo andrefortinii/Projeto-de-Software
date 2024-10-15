7f9584e7524326801c302afebc3d845d440c6e25
André Fortini <118738072+andrefortinii@users.noreply.github.com>
2024-10-14 21:39:05 -0300

    Delete projeto directory

--- projeto/testinhe/src/hooks/useAuth.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../contexts/auth"; // Certifique-se de usar o caminho correto
-
-const useAuth = () => {
-  return useContext(AuthContext);
-};
-
-export default useAuth;
